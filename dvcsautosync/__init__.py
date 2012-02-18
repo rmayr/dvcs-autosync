6a2e03fb661098d4504b9c18f832987101034a50
Sebastian Spaeth <Sebastian@SSpaeth.de>
2012-02-18 11:23:02 +0100

    Clean up imports
    Hide the ugliness of the private jabberbot import in
    dvcsautotsync.__init__.py. Cosolidate import statements at the top
    of the file.
    
    Signed-off-by: Sebastian Spaeth <Sebastian@SSpaeth.de>

--- dvcsautosync/__init__.py
+++ dvcsautosync/__init__.py
@@ -1,1 +1,21 @@
+# -*- coding: utf-8 -*-
+# ============================================================================
+# Copyright René Mayrhofer, 2012-
+#
+# Contributors:
+# * Sebastian Spaeth: refactoring & cleanup
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation; either version 2 or 3 of the License.
+# ============================================================================
+__all__ = ["Notifier", "jabberbot"]
+import warnings
+
+# Make 'Notifier' available
+from .filenotifier import Notifier
+# Make private copy of jabberbot available, disable deprecation warnings
+with warnings.catch_warnings():
+    warnings.filterwarnings("ignore",category=DeprecationWarning)
+    from . import jabberbot
 
